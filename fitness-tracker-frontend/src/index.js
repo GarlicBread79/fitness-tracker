80dd085298a3b7fe8501e9d056147704f5b04b39
GarlicBread79 <116307648+GarlicBread79@users.noreply.github.com>
2025-04-01 12:44:07 -0500

    Updated index.js

--- fitness-tracker-frontend/src/index.js
+++ fitness-tracker-frontend/src/index.js
@@ -1,10 +1,10 @@
 // src/index.js
 
-import 'bootswatch/dist/flatly/bootstrap.min.css'; // <-- Use Flatly theme
+import 'bootswatch/dist/flatly/bootstrap.min.css'; // Imports the Flatly theme
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // your custom global CSS
+import './index.css'; // Will also import the global CSS
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
