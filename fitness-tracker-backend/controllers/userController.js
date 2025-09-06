39aa5672f02634c9347a0b0d22fef3ce5002107a
GarlicBread79 <116307648+GarlicBread79@users.noreply.github.com>
2025-09-06 12:15:33 -0500

    Update userController.js

--- fitness-tracker-backend/controllers/userController.js
+++ fitness-tracker-backend/controllers/userController.js
@@ -1,7 +1,7 @@
 // controllers/userController.js
 
 const User = require('../models/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 /**
