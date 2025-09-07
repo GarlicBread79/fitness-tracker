008087265ce97760727f1a71459fdf369bb3f8f0
GarlicBread79 <116307648+GarlicBread79@users.noreply.github.com>
2025-09-07 15:05:30 -0500

    Update LoginForm.js

--- fitness-tracker-frontend/src/components/LoginForm.js
+++ fitness-tracker-frontend/src/components/LoginForm.js
@@ -6,7 +6,7 @@ function LoginForm({ onLogin }) {
 
   const onSubmit = async (data) => {
     try {
-      const response = await fetch('http://localhost:5000/api/users/login', {
+      const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/users/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data), // data = { email, password }
